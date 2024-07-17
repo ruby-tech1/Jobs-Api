03f12b43f73590f0f24e1c19e863abe0c045da1b
Ruby Tech <reubenalabi2006@gmail.com>
2024-07-17 22:43:57 +0100

    test

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("*", (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
